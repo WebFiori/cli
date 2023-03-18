159d7ead79e078a00bcd49a3996d469da8412974
Ibrahim BinAlshikh <usernane@users.noreply.github.com>
2023-03-19 02:04:45 +0300

    Update HelloWorldCommand.php

--- example/HelloWorldCommand.php
+++ example/HelloWorldCommand.php
@@ -4,11 +4,22 @@ use webfiori\cli\CLICommand;
 
 class HelloWorldCommand extends CLICommand {
     public function __construct() {
-        parent::__construct('hello');
+        parent::__construct('hello', [
+            '--person-name' => [
+                'description' => 'Name of someone to greet.',
+                'optional' => true
+            ]
+        ], 'A command to show greetings.');
     }
 
     public function exec(): int {
-        $this->println("Hello World!");
+        $name = $this->getArgValue('--person-name');
+
+        if ($name === null) {
+            $this->println("Hello World!");
+        } else {
+            $this->println("Hello %s!", $name);
+        }
 
         return 0;
     }
