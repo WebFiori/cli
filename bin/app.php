25b84d843756bf1533f1951629e378f81b63ff34
Ibrahim BinAlshikh <usernane@users.noreply.github.com>
2023-08-17 16:23:10 +0300

    Update app.php

--- bin/app.php
+++ bin/app.php
@@ -1,10 +1,11 @@
 <?php
 
-require_once '../vendor/autoload.php';
+include $_composer_autoload_path ?? __DIR__ . '/../vendor/autoload.php';
 
 use webfiori\cli\commands\HelpCommand;
 use webfiori\cli\Runner;
 
+require 'InitAppCommand.php';
 $runner = new Runner();
 $runner->register(new HelpCommand());
 $runner->register(new InitAppCommand());
