e5d818e89c9c7688ca2485bcc35530e0314a8bbe
Ibrahim BinAlshikh <usernane@users.noreply.github.com>
2023-08-17 16:23:15 +0300

    Update InitAppCommand.php

--- bin/InitAppCommand.php
+++ bin/InitAppCommand.php
@@ -2,6 +2,7 @@
 
 use webfiori\cli\CLICommand;
 use webfiori\cli\CommandArgument;
+use webfiori\file\File;
 /**
  * A class which is used to initialize a new CLI application.
  *
@@ -16,6 +17,24 @@ class InitAppCommand extends CLICommand {
     }
     public function exec(): int {
         $dirName = $this->getArgValue('--dir');
-        $this->println(__DIR__);
+        $appPath = substr(__DIR__, 0, strlen(__DIR__) - strlen('vendor\webfiori\cli\bin')).$dirName;
+        
+        $file = new File($appPath.DIRECTORY_SEPARATOR.'app.php');
+        $file->append("<?php\n\n");
+        $file->append("namespace $dirName;\n\n");
+        $file->append("//Entry point of your application.\n\n");
+        $file->append("use webfiori\cli\Runner;\n");
+        $file->append("use webfiori\cli\commands\HelpCommand;\n\n");
+        
+        
+        $file->append("\$runner = new Runner();\n");
+        $file->append("//TODO: Register Commands.\n");
+        $file->append("\$runner->register(new HelpCommand());\n\n");
+        $file->append("//Start your application.\n");
+        $file->append("\$runner->start();\n\n");
+        
+        $file->write(false, true);
+        $this->success('App created successfully.');
+        return 0;
     }
 }
