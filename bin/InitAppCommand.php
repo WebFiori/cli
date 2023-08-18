dc595dbc5a59786143b9881b2cfac5d828c5a2dc
Ibrahim BinAlshikh <usernane@users.noreply.github.com>
2023-08-18 20:16:29 +0300

    Update InitAppCommand.php

--- bin/InitAppCommand.php
+++ bin/InitAppCommand.php
@@ -11,25 +11,33 @@ use webfiori\file\File;
 class InitAppCommand extends CLICommand {
     public function __construct() {
         parent::__construct('init', [
-            new CommandArgument('--dir', 'The name of application root directory.')
+            new CommandArgument('--dir', 'The name of application root directory.'),
+            new CommandArgument('--entry', 'The name of entry point that is used to execute the application. Default value is application directory name.', true)
         ], 'Initialize new CLI application.');
 
     }
     public function exec(): int {
         $dirName = $this->getArgValue('--dir');
+        $entry = $this->getArgValue('--entry');
+        if ($entry === null) {
+            $entry = $dirName;
+        }
         $appPath = substr(__DIR__, 0, strlen(__DIR__) - strlen('vendor\webfiori\cli\bin')).$dirName;
-        $this->createAppClass($appPath);
-        $this->createEntryPoint($appPath, $dirName);
+        $this->createAppClass($appPath, $dirName);
+        $this->createEntryPoint($appPath, $dirName, $entry);
+        $this->success('App created successfully.');
         return 0;
     }
-    private function createEntryPoint(string $appPath, string $dir) {
+    private function createEntryPoint(string $appPath, string $dir, string $eName) {
+        $this->success('Creating "'.$dir.'/'.$eName.'"...');
         $file = new File($dir, $appPath);
         $file->append("#!/usr/bin/env php\n"
                 . "<?php\n"
                 . "require \"app.php\";\n\n");
         $file->write();
     }
-    private function createAppClass(string $appPath) {
+    private function createAppClass(string $appPath, string $dirName) {
+        $this->println('Creating "'.$dirName.'/app.php" ...');
         $file = new File($appPath.DIRECTORY_SEPARATOR.'app.php');
         $file->append("<?php\n\n");
         $file->append("namespace $dirName;\n\n");
@@ -45,6 +53,6 @@ class InitAppCommand extends CLICommand {
         $file->append("\$runner->start();\n\n");
         
         $file->write(false, true);
-        $this->success('App created successfully.');
+        
     }
 }
