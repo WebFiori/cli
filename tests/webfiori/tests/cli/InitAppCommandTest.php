9ba9b9efa8b87af5903828ef09c3abdbc7914928
Ibrahim BinAlshikh <usernane@users.noreply.github.com>
2023-08-18 20:16:38 +0300

    Create InitAppCommandTest.php

---
+++ tests/webfiori/tests/cli/InitAppCommandTest.php
@@ -0,0 +1,18 @@
+<?php
+namespace webfiori\tests\cli;
+
+
+use PHPUnit\Framework\TestCase;
+use webfiori\cli\CommandArgument;
+use webfiori\cli\Runner;
+
+/**
+ * Description of InitAppCommandTest
+ *
+ * @author Ibrahim
+ */
+class InitAppCommandTest extends TestCase {
+    public function test00() {
+        $this->assertTrue(true);
+    }
+}
