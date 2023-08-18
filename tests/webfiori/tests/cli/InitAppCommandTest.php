72d35d20c1a7960cf5358c93a207814f0f94afe3
Ibrahim BinAlshikh <usernane@users.noreply.github.com>
2023-08-19 00:46:56 +0300

    Update InitAppCommandTest.php

--- tests/webfiori/tests/cli/InitAppCommandTest.php
+++ tests/webfiori/tests/cli/InitAppCommandTest.php
@@ -1,9 +1,8 @@
 <?php
 namespace webfiori\tests\cli;
 
-
+use InitAppCommand;
 use PHPUnit\Framework\TestCase;
-use webfiori\cli\CommandArgument;
 use webfiori\cli\Runner;
 
 /**
@@ -12,7 +11,83 @@ use webfiori\cli\Runner;
  * @author Ibrahim
  */
 class InitAppCommandTest extends TestCase {
+    /**
+     * @test
+     */
     public function test00() {
-        $this->assertTrue(true);
+        $r = new Runner();
+        $r->register(new InitAppCommand());
+        $r->setDefaultCommand('init');
+        $r->setInputs([]);
+        $r->setArgsVector([
+            'app.php',
+            'init'
+        ]);
+        $r->start();
+        $this->assertEquals([
+            "Error: The following required argument(s) are missing: '--dir'\n"
+        ], $r->getOutput());
+    }
+    /**
+     * @test
+     */
+    public function test01() {
+        $r = new Runner();
+        $r->register(new InitAppCommand());
+        $r->setDefaultCommand('init');
+        $r->setInputs([]);
+        $r->setArgsVector([
+            'app.php',
+            'init',
+            '--dir' => 'test?'
+        ]);
+        $r->start();
+        $this->assertEquals([
+            "Creating \"test?/app.php\"...\n",
+            "Error: Unable to initialize due to an exception:\n",
+            "2 - mkdir(): Invalid path\n"
+        ], $r->getOutput());
+    }
+    /**
+     * @test
+     */
+    public function test02() {
+        $r = new Runner();
+        $r->register(new InitAppCommand());
+        $r->setDefaultCommand('init');
+        $r->setInputs([]);
+        $r->setArgsVector([
+            'app.php',
+            'init',
+            '--dir' => 'test'
+        ]);
+        $r->start();
+        $this->assertEquals([
+            "Creating \"test/app.php\"...\n",
+            "Creating \"test/test.sh\"...\n",
+            "Success: App created successfully.\n"
+        ], $r->getOutput());
+    }
+    /**
+     * @test
+     */
+    public function test03() {
+        $r = new Runner();
+        $r->register(new InitAppCommand());
+        $r->setDefaultCommand('init');
+        $r->setInputs([]);
+        $r->setArgsVector([
+            'app.php',
+            'init',
+            '--dir' => 'test'
+        ]);
+        $r->start();
+        $this->assertEquals([
+            "Creating \"test/app.php\"...\n",
+            "Warning: File app.php already exist!\n",
+            "Creating \"test/test.sh\"...\n",
+            "Warning: File test.sh already exist!\n",
+            "Success: App created successfully.\n"
+        ], $r->getOutput());
     }
 }
