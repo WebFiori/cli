c6ee5edd4c9647e77f6ffe494da8942dbd7dfb10
Ibrahim BinAlshikh <usernane@users.noreply.github.com>
2025-08-16 21:28:46 +0300

    Update main.php

--- example/app/main.php
+++ example/app/main.php
@@ -6,6 +6,7 @@ use WebFiori\Cli\Runner;
 require_once '../../vendor/autoload.php';
 require_once './HelloWorldCommand.php';
 require_once './OpenFileCommand.php';
+require_once './ProgressDemoCommand.php';
 
 
 
@@ -14,6 +15,7 @@ $runner = new Runner();
 $runner->register(new HelpCommand());
 $runner->register(new HelloWorldCommand());
 $runner->register(new OpenFileCommand());
+$runner->register(new ProgressDemoCommand());
 $runner->setDefaultCommand('help');
 
 exit($runner->start());
