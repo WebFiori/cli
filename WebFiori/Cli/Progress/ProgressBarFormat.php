58ee9d42795d7c5250fc3a5b6b1a5881e4f00676
Ibrahim BinAlshikh <usernane@users.noreply.github.com>
2025-08-19 23:38:19 +0300

    Update ProgressBarFormat.php

--- WebFiori/Cli/Progress/ProgressBarFormat.php
+++ WebFiori/Cli/Progress/ProgressBarFormat.php
@@ -49,9 +49,10 @@ class ProgressBarFormat {
             return '--:--';
         }
 
-        $hours = floor($seconds / 3600);
-        $minutes = floor(($seconds % 3600) / 60);
-        $secs = floor($seconds % 60);
+        $totalSeconds = (int) $seconds;
+        $hours = intdiv($totalSeconds, 3600);
+        $minutes = intdiv($totalSeconds % 3600, 60);
+        $secs = $totalSeconds % 60;
 
         if ($hours > 0) {
             return sprintf('%02d:%02d:%02d', $hours, $minutes, $secs);
